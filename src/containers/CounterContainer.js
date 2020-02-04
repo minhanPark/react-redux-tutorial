28a031d6a8ad44f7f688f539aaf9970a2e4ebde5
unknown <alsgks1218@naver.com>
2020-02-04 18:06:14 +0900

    saved

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 import Counter from "../components/Counter";
 
-const CounterContainer = () => {
-  return <Counter />;
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
 };
+
+const mapStateToProps = state => ({
+  number: state.counter.number
+});
+
+export default CounterContainer;
